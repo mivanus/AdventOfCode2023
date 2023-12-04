bdee1879771f9cc624a8d32c65a67e47d5361b57
mivanus <mivanus@outlook.com>
2023-12-04 20:22:52 +0100

    day4 solution

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,6 +4,8 @@ import day2.Day2Star1
 import day2.Day2Star2
 import day3.Day3Star1
 import day3.Day3Star2
+import day4.Day4Star1
+import day4.Day4Star2
 
 fun main(args: Array<String>) {
     Day1Star1().solve()
@@ -12,4 +14,6 @@ fun main(args: Array<String>) {
     Day2Star2().solve()
     Day3Star1().solve()
     Day3Star2().solve()
+    Day4Star1().solve()
+    Day4Star2().solve()
 }
